e61e1f488ebc3a0f411c3d64368737ec95062a31
wytheking <wythe523@163.com>
2022-05-20 23:27:07 +0800

    feat(svg-icon): svg-sprite-loader

--- src/icons/index.js
+++ src/icons/index.js
@@ -9,7 +9,7 @@ const svgRequire = require.context('./svg', false, /\.svg$/)
 // 此时返回了一个 Require 函数，可以接受一个 request 的参数，用于 require 的导入
 // 该函数提供了三个属性，可以通过 svgRequire.keys() 获得所有的 svg 图标
 // 遍历图标，把图标作为 request 参数传入到 svgRequire 导入函数中，完成本地 svg 图标的导入
-svgRequire.keys().forEach(SvgIcon => svgRequire(SvgIcon))
+svgRequire.keys().forEach(svgIcon => svgRequire(svgIcon))
 
 export default app => {
   app.component('svg-icon', SvgIcon)
